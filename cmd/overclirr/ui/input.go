072d667d4085a7db1b8211d088ad0491c92d01bf
Will Fantom <willfantom@gmail.com>
2021-05-02 09:44:53 +0100

    Added destructive action confirmation 💣
    Signed-off-by: Will Fantom <willfantom@gmail.com>

--- cmd/overclirr/ui/input.go
+++ cmd/overclirr/ui/input.go
@@ -1,6 +1,9 @@
 package ui
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/manifoldco/promptui"
 	"github.com/sirupsen/logrus"
 )
@@ -30,4 +33,24 @@ func GetInput(title string, validator func(string) error) (string, error) {
 		return "", err
 	}
 	return result, nil
+}
+
+func DestructiveConfirmation() {
+	prompt := promptui.Prompt{
+		Label: "This is a destructive action, do you want to continue? (yes/no)",
+		Validate: func(in string) error {
+			in = strings.ToLower(in)
+			if in != "yes" && in != "no" {
+				return errors.New("not a valid answer")
+			}
+			return nil
+		},
+	}
+	result, err := prompt.Run()
+	if err != nil {
+		logrus.WithField("extended", err.Error()).Fatalln("destructive confirmation failed")
+	}
+	if strings.ToLower(result) == "no" {
+		logrus.Fatalln("refused to accept destructive confirmation")
+	}
 }
